fce6fd2aa206a07d941b18a48e29fed3b6481b2d
rahullb <rahulb@sahajsoft.com>
2020-08-28 00:06:07 +0530

    add theme-ui

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,56 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import { useColorMode, Flex } from "theme-ui";
 
 const Header = ({ siteTitle }) => {
-  
-  return (<header
-    // style={{
-    //   background: `rebeccapurple`,
-    //   marginBottom: `1.45rem`,
-    // }}
-  >
-    <div
+  const [colorMode, setColorMode] = useColorMode();
+
+  const nextColorMode = colorMode === "light" ? "dark" : "light";
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+          <button style={{float: 'right'}}
+          onClick={(e) => {
+            setColorMode(nextColorMode);
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <label>
-            <input
-              type="checkbox"
-              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-              checked={theme === 'dark'}
-            />{' '}
-            Dark mode
-          </label>
-        )}
-      </ThemeToggler>
-    </div>
-  </header>)}
+          M
+        </button>
+        </h1>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,20 @@
+export default {
+    initialColorModeName: 'light',
+    fonts: {
+      body: 'system-ui, sans-serif',
+      heading: '"Avenir Next", sans-serif',
+      monospace: 'Menlo, monospace',
+    },
+    colors: {
+      text: '#000',
+      background: '#fff',
+      primary: '#33e',
+      modes: {
+        dark: {
+          text: '#fff',
+          background: '#000',
+          primary: '#cc1'
+        }
+      }
+    },
+  }
